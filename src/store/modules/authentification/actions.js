bd5d1c39df2fe73fea83ec8aef4626e23856c8b4
Marwin Wiechert <marwinwiechert@icloud.com>
2022-11-23 12:09:44 +0100

    Add step4 of registration form

--- src/store/modules/authentification/actions.js
+++ src/store/modules/authentification/actions.js
@@ -2,16 +2,25 @@ import {Auth} from "aws-amplify";
 import router from "@/router";
 
 
-
 export default {
     loginUser({commit}, user) {
         commit("USER_LOGIN", user)
     },
-    async logoutUser({commit}){
+    async logoutUser({commit}) {
         await Auth.signOut({
             global: true
         })
         commit("USER_LOGOUT")
         router.push("/")
+    },
+    async signUp({ commit }, form) {
+        const user = await Auth.signUp({
+            username: form.username,
+            password: form.password,
+            attributes: {
+                name: form.name
+            }
+        });
+        commit("USER_SIGNUP", user);
     }
 }
--- src/store/modules/authentification/mutations.js
+++ src/store/modules/authentification/mutations.js
@@ -7,4 +7,8 @@ export default {
         state.loggedIn = false
         state.user = undefined;
     },
+    USER_SIGNUP(state, user){
+        state.user = user;
+
+    }
 }
